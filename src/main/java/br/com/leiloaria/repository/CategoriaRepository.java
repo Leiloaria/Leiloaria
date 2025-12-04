b9631f84c7136f0c18a41afe005a9f6e9a126a8b
DavyEugenio <davy.eugenio@hotmail.com>
2025-12-04 19:03:11 +0000

    feat: instalcao de QueryDSL

--- src/main/java/br/com/leiloaria/repository/CategoriaRepository.java
+++ src/main/java/br/com/leiloaria/repository/CategoriaRepository.java
@@ -2,10 +2,21 @@ package br.com.leiloaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.leiloaria.model.Categoria;
+import br.com.leiloaria.model.QCategoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>, QuerydslPredicateExecutor<Categoria>,
+	PagingAndSortingRepository<Categoria, Long>,
+	QuerydslBinderCustomizer<QCategoria>{
 
-}
+	@Override
+	default void customize(QuerydslBindings bindings, QCategoria root) {
+		bindings.bind(root.nome).first(
+            (path, value) -> path.containsIgnoreCase(value));
+	}}
