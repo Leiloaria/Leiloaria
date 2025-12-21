bcb8442334629449833edc1fa4ffcd4fc9f27ce2
Lucas Emanoel <lucas2014.barros@gmail.com>
2025-12-21 14:14:19 -0300

    feat: load dotenv appropriately

--- src/main/java/br/com/leiloaria/LeiloariaApplication.java
+++ src/main/java/br/com/leiloaria/LeiloariaApplication.java
@@ -3,10 +3,15 @@ package br.com.leiloaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LeiloariaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+	  dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+	    
 		SpringApplication.run(LeiloariaApplication.class, args);
 	}
 
